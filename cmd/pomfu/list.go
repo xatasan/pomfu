f0c19bbe213acab71660b6ffa6528243f80ba209
Xatasan <xatasan@firemail.cc>
2018-05-27 11:20:08 +0200

    Removed header-line from pomfu(1) output

--- cmd/pomfu/list.go
+++ cmd/pomfu/list.go
@@ -12,7 +12,7 @@ import (
 const unit = 1 << 10
 
 // convert a byte count to a human readable representation
-func byteSize(bytes int) string {
+func byteSize(bytes int64) string {
 	if bytes < unit {
 		return fmt.Sprintf("%dB", bytes)
 	}
@@ -27,9 +27,6 @@ func byteSize(bytes int) string {
 // about
 func list() {
 	out := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.DiscardEmptyColumns)
-	fmt.Fprintf(out, "%s\t%s\t%s\t%s\t%s\t%s\t%s\n",
-		"key", "name", "about", "max size", "enabled",
-		"html", "owner")
 	for name, pomf := range pomfu.Servers {
 		fmt.Fprintf(out, "%s\t%s\t%s\t%s\t%t\t%t\t%s\n",
 			name, pomf.Name, pomf.About, byteSize(pomf.MaxSize), !pomf.Disabled,
